b903e342f721b7c3b1afeb5f42f96160a0ea2dcf
AlphaPaulCode <erioroegbelughepaul@gmail.com>
2025-03-29 11:50:58 +0100

    final commit

--- frontend-test/next.config.js
+++ frontend-test/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enables static site export for GitHub Pages
+    output: 'export', // Enables static site export
+    images: {
+      unoptimized: true, // Fix image issues in static exports
+    },
+    basePath: "/frontend-test", // Change this to match your GitHub repo name
+    assetPrefix: "/frontend-test/", // Ensure assets load correctly on GitHub Pages
   };
   
   module.exports = nextConfig;
